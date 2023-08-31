3e40d73d576cdfabb4ceceb645b9f7c95d87ddd2
AliveDax <69616774+AliveDax@users.noreply.github.com>
2023-08-31 17:26:55 +0530

    Add files via upload

---
+++ 04_input.c
@@ -0,0 +1,14 @@
+/*
+This is a Multi line comment
+*/
+
+#include<stdio.h>
+
+int main()
+{
+    printf("Enter your number\n");
+    int a;
+    scanf("%d", &a);
+    printf("Your number is %d\n", a);
+    return 0;
+}
